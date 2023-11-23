5fd41902436fc63dc2c8810bc75374703675a802
Tiosphere <loong_22@hotmail.com>
2023-11-23 14:15:18 +0700

    working on db connection

---
+++ backend/db/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    credit = Column(Integer, default=100)
+
+    orders = relationship("Order", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Integer)
+    quantity = Column(Integer)
+
+    orders = relationship("Order", back_populates="item")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    item_id = Column(Integer, ForeignKey("items.id"))
+    quantity = Column(Integer)
+    process = Column(Integer)
+    status = Column(String)
+
+    owner = relationship("User", back_populates="orders")
+    item = relationship("Items", back_populates="orders")
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,23 @@
-from typing import Union
+from typing import Any, Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    context: dict[str, Any] = {
+        "request": request,
+    }
+    return templates.TemplateResponse("views/home.html", context)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/order/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
