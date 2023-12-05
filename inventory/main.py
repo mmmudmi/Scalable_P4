0f7d82fed00c961354983e5fca9e3562f7051c79
Mudmi <mmmudmi@gmail.com>
2023-12-05 23:39:16 +0700

    Added Trace using jaeger

--- inventory/main.py
+++ inventory/main.py
@@ -4,8 +4,10 @@ from typing import Any
 from celery import Celery
 from celery.utils.log import get_task_logger
 from dotenv import load_dotenv
+from opentelemetry import trace
 
 from db import schemas, database, models
+tracer = trace.get_tracer(__name__)
 
 load_dotenv()
 celery = Celery(
@@ -46,34 +48,36 @@ def delete():
 
 @celery.task(name="process")
 def process(order_data: dict[str, Any]):
-    order: schemas.Order = schemas.Order.model_validate(order_data, strict=True)
-    item = db_session.query(models.Item).filter(models.Item.name == order.item).first()
-    if item is None:
-        order.status = "Invalid Item"
-        send_rollback(order.model_dump())
-        return "Invalid Item"
-    if int(item.quantity) < order.amount:
-        order.status = "Out of Stock"
-        send_rollback(order.model_dump())
-        return "Out of Stock"
+    with tracer.start_as_current_span("inventorySpan"):
+        order: schemas.Order = schemas.Order.model_validate(order_data, strict=True)
+        item = db_session.query(models.Item).filter(models.Item.name == order.item).first()
+        if item is None:
+            order.status = "Invalid Item"
+            send_rollback(order.model_dump())
+            return "Invalid Item"
+        if int(item.quantity) < order.amount:
+            order.status = "Out of Stock"
+            send_rollback(order.model_dump())
+            return "Out of Stock"
 
-    item.quantity -= order.amount
-    db_session.query(models.Item).filter(models.Item.id == item.id).update(
-        schemas.Item.model_validate(item).model_dump()
-    )
-    db_session.commit()
-    send_process(order_data)
-    return True
+        item.quantity -= order.amount
+        db_session.query(models.Item).filter(models.Item.id == item.id).update(
+            schemas.Item.model_validate(item).model_dump()
+        )
+        db_session.commit()
+        send_process(order_data)
+        return True
 
 
 @celery.task(name="rollback")
 def rollback(order_data: dict[str, Any]):
-    order: schemas.Order = schemas.Order.model_validate(order_data, strict=True)
-    item = db_session.query(models.Item).filter(models.Item.name == order.item).first()
-    item.quantity += order.amount
-    db_session.query(models.Item).filter(models.Item.id == item.id).update(
-        schemas.Item.model_validate(item).model_dump()
-    )
-    db_session.commit()
-    send_rollback(order_data)
-    return True
+    with tracer.start_as_current_span("inventoryRollBackSpan"):
+        order: schemas.Order = schemas.Order.model_validate(order_data, strict=True)
+        item = db_session.query(models.Item).filter(models.Item.name == order.item).first()
+        item.quantity += order.amount
+        db_session.query(models.Item).filter(models.Item.id == item.id).update(
+            schemas.Item.model_validate(item).model_dump()
+        )
+        db_session.commit()
+        send_rollback(order_data)
+        return True
